87bbe8f677c1eca9de96e973b048bb7e876fb5cd
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-07-26 15:00:25 +0200

    server and local time

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -185,8 +185,10 @@ const Home = () => {
                     header={header}
                     tableStyle={{ minWidth: '50rem' }}
                 >
-                    <Column field="from_local_time" header="From Server Time"></Column>
-                    <Column field="to_local_time" header="To Server Time"></Column>
+                    <Column field="from_server_time" header="From Server Time"></Column>
+                    <Column field="to_server_time" header="To Server Time"></Column>
+                    <Column field="from_local_time" header="From Local Time"></Column>
+                    <Column field="to_local_time" header="To Local Time"></Column>
                     <Column field="from_utc_time" header="From UTC Time"></Column>
                     <Column field="to_utc_time" header="To UTC Time"></Column>
                     <Column field="channel" header="Channel"></Column>
